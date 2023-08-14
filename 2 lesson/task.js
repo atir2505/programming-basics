9e6a4c42b3a2e6593a5346e1e56a3cbb73410bb2
atir2505 <142150739+atir2505@users.noreply.github.com>
2023-08-14 20:10:57 +0200

    name

---
+++ 2 lesson/task.js
@@ -0,0 +1,12 @@
+const firstNumber = 7
+const secondNumber = 3
+const sum = firstNumber + secondNumber
+console.log (sum)
+const firstName = "Oleg"
+const secondName = "Zavrotskyi"
+const sum2 = firstName + secondName
+console.log (sum2)
+const numberOne = 2004
+const numberTwo = "Leonid"
+const sum3 = numberOne + numberTwo
+console.log (sum3)
