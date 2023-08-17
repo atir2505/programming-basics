197b8ccace6af5a60473ccddf57cd34b7f8414a4
atir2505 <142150739+atir2505@users.noreply.github.com>
2023-08-17 22:13:46 +0200

    lesson 2.2

--- 2 lesson/task.js
+++ 2 lesson/task.js
@@ -1,12 +1,6 @@
 const firstNumber = 7
 const secondNumber = 3
-const sum = firstNumber + secondNumber
-console.log (sum)
-const firstName = "Oleg"
-const secondName = "Zavrotskyi"
-const sum2 = firstName + secondName
-console.log (sum2)
-
+//const sum = firstNumber + secondNumber
 const oneNumber = 100000000
 const twoNumber = 946200
 const difference = oneNumber - twoNumber
@@ -15,3 +9,29 @@ const nameOne = "Irina"
 const nameTwo ="Kryklyva"
 const sumName = nameOne + nameTwo
 console.log (sumName)
+console.log("leonid")
+const firstName = "leonid" 
+
+const secondName = "SIvnitskyi"
+const plus = firstName + " " + secondName
+console.log(plus)
+const letter = "1"
+const num = 1
+const sum = letter + num
+console.log(sum)
+const x = 32
+const y = 0.45
+const z = -8
+const operation1 = (x + y) / z
+const operation2 = 2 * z / (x + y)
+const operation3 = 100 - ((x + y) / (y + z) * (x + z) / ( x +y )) 
+const operation4 = x**y
+console.log ("car" + true)
+const myName = "Leonid"
+const surname = "Sivnitskyi"
+const birthYear = 2004
+const age = 2023
+const diefference = age - birthYear
+const data = myName + " " + surname + " " + diefference
+console.log(data)
+
